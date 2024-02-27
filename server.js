15b8d0e4af07adf04f3855e9273dc5c021f8724a
Einanas <inez.el.azzouzi@hva.nl>
2024-02-28 00:28:57 +0100

    nieuwe poging bij de server.js cuz patience runs faster than me

--- server.js
+++ server.js
@@ -1,3 +1,103 @@
+/***  express setup & start code ***/
+
+
+/* ophalen van stuff */
+// Importeer het npm pakket express uit de node_modules map
+// import express from 'express'
+
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+// import fetchJson from './helpers/fetch-json.js'
+
+// Stel het basis endpoint in
+// const apiUrl = 'https://fdnd.directus.app/items'
+
+// Haal alle squads uit de WHOIS API op
+// const squadData = await fetchJson(apiUrl + '/squad')
+
+// Stel de map met ejs templates in
+// app.set('views', './views')
+
+// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+// app.use(express.static('public'))
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+// app.set('port', process.env.PORT || 8000)
+
+// Maak een nieuwe express app aan
+// const app = express()
+
+// Stel ejs in als template engine
+// app.set('view engine', 'ejs')
+
+
+
+/*** Routes & data ***/
+
+// const squadData = await fetchJson(baseURL + 'squad')
+
+// array klaar zetten voor waarin globale berichten voor message board op komt te staan
+// const messages = []
+
+
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+// app.listen(app.get('port'), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  // console.log(`Application started on http://localhost:${app.get('port')}`)
+// })
+
+// Maak een GET route voor de index
+// app.get('/', function (request, response)) {
+  // Haal alle personen uit de WHOIS API op
+  // fetchJson(apiUrl + '/person/?filter={"squad_id":4}').then((apiData) => {
+    // apiData bevat gegevens van alle personen uit alle squads
+    // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
+
+    // Render index.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
+    // response.render('index', {
+      // persons: apiData.data, 
+      // squads: squadData.data,
+      // messages: messages
+    // })
+  // })
+// })
+
+// Maak een POST route voor de index
+// app.post('/', function (request, response) {
+  // voeg het nieuwe bericht toe aan de messages array
+  // messages.push(request.body.bericht)
+
+  // Er is nog geen afhandeling van POST, redirect naar GET op /
+  // response.redirect(303, '/')
+// })
+
+// Maak een GET route voor een detailpagina met een request parameter id
+// app.get('/person/:id', function (request, response) {
+  // Gebruik de request parameter id en haal de juiste persoon uit de WHOIS API op
+  // fetchJson(apiUrl + '/person/' + request.params.id).then((apiData) => {
+    // Render person.ejs uit de views map en geef de opgehaalde data mee als variable, genaamd person
+    // response.render('person', {person: apiData.data, squads: squadData.data})
+  // })
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Old code cuz i did someting wrong and i dont know what
+
+
+
+
 // Importeer het npm pakket express uit de node_modules map
 import express from 'express'
 
@@ -22,6 +122,11 @@ app.set('views', './views')
 // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static('public'))
 
+// Zorg dat werken met request data makkelijker wordt
+app.use(express.urlencoded({extended: true}))
+
+const message = []
+
 // Maak een GET route voor de index
 app.get('/', function (request, response) {
   // Haal alle personen uit de WHOIS API op
@@ -30,12 +135,19 @@ app.get('/', function (request, response) {
     // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
 
     // Render index.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
-    response.render('index', {persons: apiData.data, squads: squadData.data})
+    response.render('index', {
+      persons: apiData.data, 
+      squads: squadData.data,
+      messages: message
+    })
   })
 })
 
 // Maak een POST route voor de index
 app.post('/', function (request, response) {
+  // Voeg het nieuwe bericht toe aan de messages array
+  message.push(request.body.bericht)
+
   // Er is nog geen afhandeling van POST, redirect naar GET op /
   response.redirect(303, '/')
 })
@@ -56,4 +168,40 @@ app.set('port', process.env.PORT || 8000)
 app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+// Als we vanuit de browser een POST doen op de detailpagina van een persoon
+app.post('/detail/:id/like-or-unlike', function (request, response) {
+
+  // STAP 1: Haal de huidige gegevens voor deze persoon op, uit de WHOIS API
+  fetchJson('https://fdnd.directus.app/items/person/' + request.params.id).then((apiResponse) => {
+    // Het custom field is een String, dus die moeten we eerst omzetten (= parsen)
+    // naar een Object, zodat we er mee kunnen werken
+    try {
+      apiResponse.data.custom = JSON.parsel(apiResponse.data.custom)
+    } catch (e) {
+      apiResponse.data.custom = {}
+    }
+
+    // STAP 2: Voeg de like toe aan het custom object...
+    if (request.body.actie == 'leuk hoor') {
+      apiResponse.data.custom.like = true
+    } else {
+      apiResponse.data.custom.like = false
+    }
+
+    // STAP 3: Overschrijf het custom field voor deze persoon
+    fetchJson('https://fdnd.directus.app/items/person/' + request.params.id, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        custom: apiResponse.data.custom
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    }).then((patchResponse) => {
+      // Redirect naar de persoon pagina
+      response.redirect(303, '/detail/' + request.params.id)
+    })
+  })
 })
